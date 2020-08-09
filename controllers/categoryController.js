d1e3be2ddd18c7705270ea01a60685ef8a2b5f44
ricardo97 <bengomezperez@gmail.com>
2020-08-09 17:48:12 -0500

    Se agrego nuevas cosas

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -80,4 +80,118 @@ module.exports.findAll = async (req, res) => {
 
 }
 
+module.exports.findOne = async (req, res) => {
+
+    try {
+
+        const category = await Category.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        return res.json({
+            category
+        });
+        
+    } catch (e) {
+
+        console.log(e);
+
+        return res.json({
+            message: 'internal_error'
+        }).status(500);
+        
+    }
+
+}
+
+module.exports.update = async (req, res) => {
+
+    try {
+
+        const {
+            name
+        } = req.body;
+
+        const validator = new Validator(req.body, {
+            name: 'required|string'
+        });
+
+        if(validator.fails()) return res.json({
+            message: 'invalid_params',
+            errors: validator.errors.errors
+        });
+
+        await Category.update({
+            name
+        }, {
+            where: {
+                id: req.params.id
+            }
+        });
+
+        const category = await Category.findOne({
+            where: {
+                id: req.params.id
+            }
+        });
+
+        return res.json({
+            message: 'updated',
+            category
+        }).status(201);
+        
+    } catch (e) {
+
+        console.log(e);
+
+        return res.json({
+            message: 'internal_error'
+        }).status(500);
+
+    }
+
+}
+
+module.exports.deleteOne = async (req, res) => {
+
+    try {
+
+        const {
+            id
+        } = req.params;
+
+        await Category.update({
+            isDeleted: 1
+        }, {
+            where: {
+                id
+            }
+        });
+
+        const category = await Category.findOne({
+            where: {
+                id
+            }
+        });
+
+        return res.json({
+            message: 'deleted',
+            category
+        }).status(201);
+        
+    } catch (e) {
+
+        console.log(e);
+
+        return res.json({
+            message: 'internal_error'
+        }).status(500);
+        
+    }
+
+}
+
+
 
